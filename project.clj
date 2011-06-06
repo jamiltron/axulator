ddd14b296e7f24f3bddf684692a02d6488b44b39
Kevin Beam <kwbeam@gmail.com>
2011-06-05 18:15:55 -0600

    Added clojure-contrib

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject axulator "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "FIXME: Axis and Allies calculator"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
