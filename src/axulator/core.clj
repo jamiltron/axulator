0de176e3eb3c3dee698417487d68c42f1b1f5738
Kevin Beam <kwbeam@gmail.com>
2011-06-06 22:13:12 -0600

    Initial tests for attack and defend levels

--- src/axulator/core.clj
+++ src/axulator/core.clj
@@ -1,1 +1,5 @@
 (ns axulator.core)
+
+(def attack {:infantry 1, :tank 3, :fighter 3, :bomber 4, :submarine 2, :transport 0, :carrier 1, :aa-gun 0})
+
+(def defend {:infantry 2, :tank 2, :fighter 4, :bomber 1, :submarine 2, :transport 1, :carrier 3, :aa-gun 1})
--- test/axulator/test/core.clj
+++ test/axulator/test/core.clj
@@ -2,5 +2,23 @@
   (:use [axulator.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest attack-tests
+  (testing "infantry" (is (= 1 (:infantry attack))))
+  (testing "tank" (is (= 3 (:tank attack))))
+  (testing "fighter" (is (= 3 (:fighter attack))))
+  (testing "bomber" (is (= 4 (:bomber attack))))
+  (testing "submarine" (is (= 2 (:submarine attack))))
+  (testing "transport" (is (= 0 (:transport attack))))
+  (testing "carrier" (is (= 1 (:carrier attack))))
+  (testing "aa-gun" (is (= 0 (:aa-gun attack)))))
+
+(deftest defend-tests
+  (testing "infantry" (is (= 2 (:infantry defend))))
+  (testing "tank" (is (= 2 (:tank defend))))
+  (testing "fighter" (is (= 4 (:fighter defend))))
+  (testing "bomber" (is (= 1 (:bomber defend))))
+  (testing "submarine" (is (= 2 (:submarine defend))))
+  (testing "transport" (is (= 1 (:transport defend))))
+  (testing "carrier" (is (= 3 (:carrier defend))))
+  (testing "aa-gun" (is (= 1 (:aa-gun defend)))))
+
