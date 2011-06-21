869598d8a32deb1be8a061304cea7a68b30ddf95
Kevin Beam <kwbeam@gmail.com>
2011-06-21 16:33:46 -0500

    add cost tests and implementation

--- src/axulator/core.clj
+++ src/axulator/core.clj
@@ -1,5 +1,7 @@
 (ns axulator.core)
 
-(def attack {:infantry 1, :tank 3, :fighter 3, :bomber 4, :submarine 2, :transport 0, :carrier 1, :aa-gun 0})
+(def attack {:infantry 1, :armor 3, :fighter 3, :bomber 4, :submarine 2, :transport 0, :carrier 1, :antiaircraft 0, :battleship 4})
 
-(def defend {:infantry 2, :tank 2, :fighter 4, :bomber 1, :submarine 2, :transport 1, :carrier 3, :aa-gun 1})
+(def defend {:infantry 2, :armor 2, :fighter 4, :bomber 1, :submarine 2, :transport 1, :carrier 3, :antiaircraft 1, :battleship 4})
+
+(def cost {:infantry 3, :armor 5, :fighter 12, :bomber 15, :submarine 8, :transport 8, :carrier 18, :antiaircraft 5, :battleship 24})
--- test/axulator/test/core.clj
+++ test/axulator/test/core.clj
@@ -4,21 +4,33 @@
 
 (deftest attack-tests
   (testing "infantry" (is (= 1 (:infantry attack))))
-  (testing "tank" (is (= 3 (:tank attack))))
+  (testing "armor" (is (= 3 (:armor attack))))
   (testing "fighter" (is (= 3 (:fighter attack))))
   (testing "bomber" (is (= 4 (:bomber attack))))
   (testing "submarine" (is (= 2 (:submarine attack))))
   (testing "transport" (is (= 0 (:transport attack))))
   (testing "carrier" (is (= 1 (:carrier attack))))
-  (testing "aa-gun" (is (= 0 (:aa-gun attack)))))
+  (testing "antiaircraft" (is (= 0 (:antiaircraft attack))))
+  (testing "battleship" (is (= 4 (:battleship attack)))))
 
 (deftest defend-tests
   (testing "infantry" (is (= 2 (:infantry defend))))
-  (testing "tank" (is (= 2 (:tank defend))))
+  (testing "armor" (is (= 2 (:armor defend))))
   (testing "fighter" (is (= 4 (:fighter defend))))
   (testing "bomber" (is (= 1 (:bomber defend))))
   (testing "submarine" (is (= 2 (:submarine defend))))
   (testing "transport" (is (= 1 (:transport defend))))
   (testing "carrier" (is (= 3 (:carrier defend))))
-  (testing "aa-gun" (is (= 1 (:aa-gun defend)))))
+  (testing "antiaircraft" (is (= 1 (:antiaircraft defend))))
+  (testing "battleship" (is (= 4 (:battleship defend)))))
 
+(deftest cost-tests
+  (testing "infantry" (is (= 3 (:infantry cost))))
+  (testing "armor" (is (= 5 (:armor cost))))
+  (testing "fighter" (is (= 12 (:fighter cost))))
+  (testing "bomber" (is (= 15 (:bomber cost))))
+  (testing "submarine" (is (= 8 (:submarine cost))))
+  (testing "transport" (is (= 8 (:transport cost))))
+  (testing "carrier" (is (= 18 (:carrier cost))))
+  (testing "antiaircraft" (is (= 5 (:antiaircraft cost))))
+  (testing "battleship" (is (= 24 (:battleship cost)))))
