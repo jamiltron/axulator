2114dc210ffe9a59c3b7cb7641c27ee5b3e243c0
Kevin Beam <kwbeam@gmail.com>
2011-06-25 13:59:05 -0500

    Refactor to have one function that calculates hits

--- src/axulator/core.clj
+++ src/axulator/core.clj
@@ -1,20 +1,43 @@
 (ns axulator.core)
 
-(def attack {:infantry 1, :armor 3, :fighter 3, :bomber 4, :submarine 2, :transport 0, :carrier 1, :antiaircraft 0, :battleship 4})
+(def attack {:infantry 1,
+             :armor 3,
+             :fighter 3,
+             :bomber 4,
+             :submarine 2,
+             :transport 0,
+             :carrier 1,
+             :antiaircraft 0,
+             :battleship 4})
 
-(def defend {:infantry 2, :armor 2, :fighter 4, :bomber 1, :submarine 2, :transport 1, :carrier 3, :antiaircraft 1, :battleship 4})
+(def defend {:infantry 2,
+             :armor 2,
+             :fighter 4,
+             :bomber 1,
+             :submarine 2,
+             :transport 1,
+             :carrier 3,
+             :antiaircraft 1,
+             :battleship 4})
 
-(def cost {:infantry 3, :armor 5, :fighter 12, :bomber 15, :submarine 8, :transport 8, :carrier 18, :antiaircraft 5, :battleship 24})
+(def cost {:infantry 3,
+           :armor 5,
+           :fighter 12,
+           :bomber 15,
+           :submarine 8,
+           :transport 8,
+           :carrier 18,
+           :antiaircraft 5,
+           :battleship 24})
 
-(def d6
+(def tables {:attack attack, :defend defend, :cost cost})
+
+(defn d6 []
   (repeatedly (fn [] (inc (rand-int 6)))))
 
-(defn attacker-hits [num unit die]
-  (count (filter (fn [x] (>= (unit attack) x)) (take num die))))
+(defn- max-die-roll [mode unit]
+  (unit (mode tables)))
 
-(defn defender-hits [num unit die]
-  (count (filter (fn [x] (>= (unit defend) x)) (take num die))))
-
-
-
-
+(defn hits [mode num unit die]
+  (let [max-roll (max-die-roll mode unit)]
+    (count (filter (fn [x] (<= x max-roll)) (take num die)))))
